306d09b90ac9d91dfbb1603213f4d0e74d0d7dfd
Paulo Sobral <paulo.sobral@outlook.com.br>
2022-11-14 22:37:17 -0300

    feat: add GitHub Actions

--- 01-tf/backend.tf
+++ 01-tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-blockchain-network"
+    bucket = "terraform-state-blockchain-network2"
     key    = "terraform-ec2.tfstate"
     region = "us-east-1"
   }
