67e6c023177921df4da083427b46c43bf481cd5f
Paulo Sobral <paulo.sobral@outlook.com.br>
2022-11-14 22:54:27 -0300

    change s3 backend name

--- 01-tf/backend.tf
+++ 01-tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-blockchain-network2"
+    bucket = "terraform-state-blockchain-network"
     key    = "terraform-ec2.tfstate"
     region = "us-east-1"
   }
